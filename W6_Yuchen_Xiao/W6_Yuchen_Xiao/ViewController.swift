745c5c4aefece3e842389e874a4a8a767aee5275
cj3699xiao <41243778+cj3699xiao@users.noreply.github.com>
2020-05-22 10:53:58 -0500

    Add files via upload

---
+++ W6_Yuchen_Xiao/W6_Yuchen_Xiao/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  W6_Yuchen_Xiao
+//
+//  Created by yuchen xiao on 2/23/20.
+//  Copyright © 2020 yuchen xiao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
